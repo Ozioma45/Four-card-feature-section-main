e417b50a378eee9c7259e35edc6f1aceb714e409
Ozioma45 <oziomaegole@gmail.com>
2024-03-04 04:59:50 +0100

    style the footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./styles.css";
 import FeatureSection from "./components/FeatureSection";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <FeatureSection />
+      <Footer />
     </div>
   );
 }
