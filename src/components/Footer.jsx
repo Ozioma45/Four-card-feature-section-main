67813313f465f4e47f53cc6bb861993784f22fe8
Ozioma45 <oziomaegole@gmail.com>
2024-03-04 04:57:14 +0100

    added the footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+export default function Footer() {
+  return (
+    <div className="mt-4">
+      <p className="attribution">
+        Challenge by
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          Frontend Mentor
+        </a>
+        . Coded by <a href="#">Ozioma Egole</a>.
+      </p>
+    </div>
+  );
+}
